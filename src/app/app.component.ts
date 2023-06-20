b59f679938195a1c09f3eb5138dd6b1dd6215339
cinta <cintatafur@gmail.com>
2023-06-20 18:09:31 +0200

    ✅ Create(property)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularComponentBasic';
+  public title: string = 'Mi primera app de Angular';
+  public counter: number = 10;
 }
