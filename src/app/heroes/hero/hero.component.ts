0bff2b6e932133341603ac96c1e47aac6726b440
cinta <cintatafur@gmail.com>
2023-06-28 17:26:10 +0200

    ➕ concatenación(metodos):con interpolacion

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent {
-
   public name: string = 'iroman';
-  public age:  number = 45;
+  public age: number = 45;
 
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+ /*  public */ getHeroDescription(): string {
+    return `${this.name} - ${this.age} `;
+  }
 }
