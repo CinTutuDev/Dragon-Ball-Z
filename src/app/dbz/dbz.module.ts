11394d4c1e65a7786b0d61c71f864c71f88dde7a
cinta <cintatafur@gmail.com>
2023-06-29 10:21:42 +0200

    ➕ Create(componente):creo componente list y add para modulo de dbz

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 /* Componentes */
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { ListComponent } from './components/list/list.component';
 import { MainPageComponent } from './pages/main-page.component';
 
+
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
   imports: [CommonModule],
   exports: [MainPageComponent],
 })
