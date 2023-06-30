2521485efa77f985d8ba3ed95f7201ed4fd5db56
cinta <cintatafur@gmail.com>
2023-06-30 12:37:38 +0200

    ✅ Depurando cod

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -4,27 +4,26 @@ import { Character } from '../../interfaces/character.interface';
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
-  styleUrls: ['./add-character.component.scss']
+  styleUrls: ['./add-character.component.scss'],
 })
-export class AddCharacterComponent  {
-
+export class AddCharacterComponent {
   @Output()
-  onNewCharacter: EventEmitter<Character> = new EventEmitter();
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
 
   public character: Character = {
     name: '',
-    power: 0
+    power: 0,
+  };
+
+  emitCharacter(): void {
+    /* debugger; */
+    /*  console.log(this.character); */
+
+    if (this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit({ ...this.character });
+
+    /* Para limpiar al enviar los input */
+    this.character = { name: '', power: 0 };
   }
-
-  emitCharacter(): void{
-    console.log(this.character);
-
-    if( this.character.name.length === 0) return;
-
-    this.onNewCharacter.emit({...this.character})
-
-    this.character.name = '';
-    this.character.power = 0;
-  }
-
 }
